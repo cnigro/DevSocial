4ff7de2d72ac9501ef67fc746faafc52e3ff08d3
cameron nigro <cameron.nigro@gmail.com>
2017-08-06 21:13:41 -0400

    Initial commit

---
+++ DevSocial/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevSocial
+//
+//  Created by cameron nigro on 8/6/17.
+//  Copyright © 2017 Sheepware. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
